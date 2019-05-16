0a43384b1c33c9e71fb989c60d522859506c989a
zyp001a <yzhou01a@gmail.com>
2019-05-16 12:52:14 +0800

    rm comment

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-find $1 -name "*.$2" -exec cat {} \; >tmp.txt
+find $1 -name "*.$2" -exec cat {} \; | awk '!/(^(\s*\/\/|\s*#|\s*\*|\s*\/\*)|^\s*$)/' >tmp.txt
 soffice --headless --convert-to docx tmp.txt
 rm tmp.txt
